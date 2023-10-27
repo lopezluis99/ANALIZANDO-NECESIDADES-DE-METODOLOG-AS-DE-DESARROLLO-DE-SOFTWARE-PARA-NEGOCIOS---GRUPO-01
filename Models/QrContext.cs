547385fb785ef3da07497850f5b61f60c037dd25
leonvane26 <116960268+leonvane26@users.noreply.github.com>
2023-10-27 11:14:19 -0600

    Add files via upload

---
+++ Models/QrContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace QRGenerador.Models
+{
+    public class QrContext: DbContext
+    {
+        public QrContext(DbContextOptions options): base(options) 
+        {
+        }
+        public DbSet<QRAtributo> CodigosQr { get; set; }
+
+    }
+}
