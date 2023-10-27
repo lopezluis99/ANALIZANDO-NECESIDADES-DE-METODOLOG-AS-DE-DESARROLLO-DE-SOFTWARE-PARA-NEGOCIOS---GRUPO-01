682a3444af7eb5037dcd8aed808768f063969d1a
leonvane26 <116960268+leonvane26@users.noreply.github.com>
2023-10-27 11:37:32 -0600

    Add files via upload

---
+++ QRGenerador/Models/QRAtributo.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace QRGenerador.Models
+{
+	public class QRAtributo
+	{
+		public int ID{ get; set; }
+		public string URL{ get; set; }
+		public byte[] Imagen { get; set; }
+	}
+}
